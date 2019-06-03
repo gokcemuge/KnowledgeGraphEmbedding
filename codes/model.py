c65613951e1e8f94b7448ae03087fc4871a63f9d
gokcemuge <gokce.mugee@gmaill.com>
2019-06-03 22:28:37 +0200

    New loss function implementation v13

--- codes/model.py
+++ codes/model.py
@@ -300,13 +300,13 @@ class KGEModel(nn.Module):
         '''check here'''
         # gamma1 for negative samples
         gamma1 = nn.Parameter(
-            torch.Tensor([24.0]),
+            torch.Tensor([12.0]),
             requires_grad=False
         )
 
         # gamma2 for negative samples
         gamma2 = nn.Parameter(
-            torch.Tensor([24.0]),
+            torch.Tensor([12.0]),
             requires_grad=False
         )
 
