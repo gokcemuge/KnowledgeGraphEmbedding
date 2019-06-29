5ed027834b0cd39ba75631e4603c224ade95769b
gokcemuge <gokce.mugee@gmaill.com>
2019-06-29 15:18:03 +0200

    typo fix

--- codes/run.py
+++ codes/run.py
@@ -137,7 +137,7 @@ def set_logger(args):
     Write logs to checkpoint and console
     '''
 
-    if args.do_train1:
+    if args.do_train:
         log_file = os.path.join(args.save_path or args.init_checkpoint, 'train.log')
     else:
         log_file = os.path.join(args.save_path or args.init_checkpoint, 'test.log')
