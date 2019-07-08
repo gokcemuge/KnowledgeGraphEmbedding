386b0d22d79a6da892ecd1ee0131979594e09613
gokcemuge <gokce.mugee@gmaill.com>
2019-07-09 01:02:38 +0200

    RReLU

--- codes/model.py
+++ codes/model.py
@@ -319,13 +319,13 @@ class KGEModel(nn.Module):
         if args.negative_adversarial_sampling:
             # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
             negative_score = (F.softmax(negative_score * args.adversarial_temperature, dim=1).detach()
-                              * nn.rrelu(-negative_score)).sum(dim=1)
+                              * nn.RReLU(-negative_score)).sum(dim=1)
         else:
-            negative_score = nn.rrelu(-negative_score).mean(dim=1)
+            negative_score = nn.RReLU(-negative_score).mean(dim=1)
 
         positive_score = -model(positive_sample, sampling='positive')
 
-        positive_score = nn.rrelu(positive_score).squeeze(dim=1)
+        positive_score = nn.RReLU(positive_score).squeeze(dim=1)
 
     #TODO: uni weights nedir bak
         if args.uni_weight:
